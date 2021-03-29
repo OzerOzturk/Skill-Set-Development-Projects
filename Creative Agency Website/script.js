b7c64bc43f829e95d6a15a9d48eaaadf59d0ffe4
OzerOzturk <ozerozturk1636@gmail.com>
2021-03-29 21:58:40 +0300

    toggle and navigation has edited in the way of style.

---
+++ Creative Agency Website/script.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector('.toggle')
+
+const navigation = document.querySelector('.navigation')
+
+
+toggle.addEventListener('click', () => {
+        toggle.classList.toggle('active')
+        navigation.classList.toggle('active')
+})
